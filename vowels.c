8345fbea8901594af42a5e02afb3bcb1264a438d
dusan <dusan.kojcic90@gmail.com>
2016-09-03 16:41:09 +0200

    dodao funkciju unesi_samoglasnike

--- vowels.c
+++ vowels.c
@@ -5,14 +5,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void print(char niz[150], int a, int e, int i, int o, int u, int n) {
+void print(char niz[], int a, int e, int i, int o, int u, int n) {
 puts(niz);
 printf("Recenica ima %d karaktera:\n\n", n);
 printf ("Broj samoglasnika je:\n a: %d\n e: %d\n"
         " i: %d\n o: %d\n u: %d\n", a, e, i, o, u);
 }
 
-void broj_samoglasnika(int *a, int *e, int *i, int *o, int *u, int n, char niz[150]) {
+void broj_samoglasnika(int *a, int *e, int *i, int *o, int *u, int n, char niz[]) {
 int k;
   for (k = 0; k < n; k++) {
     switch(niz[k]) {
@@ -40,12 +40,24 @@ int k;
   }
 }
 
-int velicina_n(char niz[150], int *n) {
+void unesi_samoglasnike(int a, int e, int i, int o, int u, char vowels[]) {
+  int k;
+      vowels[0] = a;
+      vowels[1] = e;
+      vowels[2] = i;
+      vowels[3] = o;
+      vowels[4] = u;
+
+  printf ("Broj samoglasnika je:\n a: %d\n e: %d\n"
+          " i: %d\n o: %d\n u: %d\n", vowels[0], vowels[1], vowels[2], vowels[3], vowels[4]);
+}
+
+int velicina_n(char niz[], int *n) {
 *n = strlen (niz);
 return *n;
 }
 
-void unesi(char niz[150]) {
+void unesi(char niz[]) {
 printf("Napisi recenicu.\n");
 char *b = niz;
 size_t bufsize = 150;
@@ -54,6 +66,7 @@ getline(&b, &bufsize, stdin);
 
 main() {
 char niz[150];
+char vowels[5];
 int n;
 int a = 0;
 int e = 0;
@@ -63,5 +76,6 @@ int u = 0;
 unesi(niz);
 velicina_n(niz, &n);
 broj_samoglasnika(&a, &e, &i, &o, &u, n, niz);
+unesi_samoglasnike(a, e, i, o, u, vowels);
 print(niz, a, e, i, o, u, n);
 }
